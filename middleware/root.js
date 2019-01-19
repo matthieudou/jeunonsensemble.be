0fe72c91c2593a6c620cad3ed4aebf50f98aaa15
matthieudou <matthieu.doultremont@gmail.com>
2019-01-19 22:30:34 +0100

    please

--- middleware/root.js
+++ middleware/root.js
@@ -1,5 +1,8 @@
 export default function ({ store, route, redirect }) {
   if (route.fullPath === '/') {
+    if (store.getters['getLinks'].length <= 0) {
+      return redirect('/accueil')
+    }
     return redirect(store.getters['getLinks'][0].slug)
   }
 }
