bbc4e521c07ee805736ac7fd469aa9de2a1c3625
matthieudou <matthieu.doultremont@gmail.com>
2019-01-19 20:32:14 +0100

    add footer to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="font-sans">
     <nuxt/>
+    <base-footer/>
   </div>
 </template>
 
 <script>
+  import BaseFooter from '~/components/shared/BaseFooter'
+
   export default {
     middleware: ['root'],
 
@@ -14,6 +17,10 @@
           class: 'bg-grey-lightest'
         }
       }
+    },
+
+    components: {
+      BaseFooter
     }
   }
 </script>
