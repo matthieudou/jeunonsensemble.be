4e70374dd637d412418dee98dec950c30df67388
matthieudou <matthieu.doultremont@gmail.com>
2019-01-19 19:57:02 +0100

    add open graph and config files

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,23 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Site web de jêunons ensemble' }
+      { name: 'msapplication-TileColor', content: '#f0f4f8' },
+      { name: 'theme-color', content: '#2680C2' },
+      { hid: 'description', name: 'description', content: 'Celui qui est affamé ne jeûne pas et celui qui jeûne n’est pas affamé' },
+      { hid: 'og:description', property: 'og:description', content: 'Celui qui est affamé ne jeûne pas et celui qui jeûne n’est pas affamé' },
+      { hid: 'og:image:width', property: 'og:image:width', content: '1145' },
+      { hid: 'og:image:height', property: 'og:image:height', content: '599' },
+      { hid: 'og:title', property: 'og:title', content: 'Jeûnons ensemble' },
+      { hid: 'og:url', property: 'og:url', content: 'https://jeunonsensemble.be' },
+      { hid: 'og:image', property: 'og:image', content: 'https://jeunonsensemble.be/og-image.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#2680C2' },
       { href: 'https://fonts.googleapis.com/css?family=Rubik', rel: 'stylesheet' }
     ]
   },
