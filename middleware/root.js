96849e46c83c06dd30b47b16ee02604670122684
matthieudou <matthieu.doultremont@gmail.com>
2019-01-19 22:09:22 +0100

    🤞 please make it work

--- middleware/root.js
+++ middleware/root.js
@@ -1,3 +1,5 @@
 export default function ({ store, route, redirect }) {
-  if (route.path === '/') redirect(store.getters['getLinks'][0].slug)
+  if (route.fullPath === '/') {
+    return redirect(store.getters['getLinks'][0].slug)
+  }
 }
